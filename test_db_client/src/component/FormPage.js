668ea8904212823d27604daf4e5071ff112eda5f
Ridhwan0406 <muhammadridhwan0404@gmail.com>
2023-09-28 14:06:48 +0800

    Fixed FormPage/UploadPage Responsive

--- test_db_client/src/component/FormPage.js
+++ test_db_client/src/component/FormPage.js
@@ -5,43 +5,43 @@ function FormPage() {
 
   const buttonStyle = {
     backgroundColor: '#491E6E',
-    borderColor: '#491E6E', // You can set the border color to match the background color if you want
-    color: 'white', // Set text color to contrast with the background
+    borderColor: '#491E6E',
+    color: 'white',
   };
   return (
     <div className="mt-5 form-page">
       <div className="form-column left">
-        <h2 className="left-header" >Borang Permohonan<br />Bantuan Khairat<br />Kematian</h2>
-        <p className="left-header-para" >#USMCares</p>
+        <h2 className="left-header">Borang Permohonan<br />Bantuan Khairat<br />Kematian</h2>
+        <p className="left-header-para">#USMCares</p>
       </div>
       <div className="form-column right">
         <h2 className="right-header">Sila Isi Borang Permohonan</h2>
         <p className="right-header-para">
-        Pastikan maklumat yang diisi tepat & sahih
+          Pastikan maklumat yang diisi tepat & sahih
         </p>
-        <form >
+        <form>
           <div className="form-group">
-            <label htmlFor="email2">Nama:</label>
-            <input type="email" id="email2" name="email2" />
+            <label htmlFor="nama">Nama:</label>
+            <input type="text" id="nama" name="nama" />
           </div>
           <div className="form-group">
-            <label htmlFor="email2">Emel:</label>
-            <input type="email" id="email2" name="email2" />
+            <label htmlFor="emel">Emel:</label>
+            <input type="email" id="emel" name="emel" />
           </div>
           <div className="form-group">
-            <label htmlFor="email2">Matric Num:</label>
-            <input type="email" id="email2" name="email2" />
+            <label htmlFor="matricNum">Matric Num:</label>
+            <input type="text" id="matricNum" name="matricNum" />
           </div>
           <div className="form-group">
-            <label htmlFor="email2">IC Num:</label>
-            <input type="email" id="email2" name="email2" />
+            <label htmlFor="icNum">IC Num:</label>
+            <input type="text" id="icNum" name="icNum" />
           </div>
           <div className="form-group">
-            <label htmlFor="email2">Year:</label>
-            <input type="email" id="email2" name="email2" />
+            <label htmlFor="year">Year:</label>
+            <input type="text" id="year" name="year" />
           </div>
           {/* Add more form fields as needed */}
-          <button type="submit" style={buttonStyle} >Submit</button>
+          <button type="submit" style={buttonStyle}>Submit</button>
         </form>
       </div>
     </div>
