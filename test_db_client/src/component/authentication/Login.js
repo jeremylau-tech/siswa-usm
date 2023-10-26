842d8a7a3762bb0729d9cbaf61f774d578b7205d
FirdausKhamid <firdauskhamid@student.usm.my>
2023-10-26 22:00:45 +0800

    UI Improvement

--- test_db_client/src/component/authentication/Login.js
+++ test_db_client/src/component/authentication/Login.js
@@ -2,17 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import React, { useState } from "react";
-import Cookies from 'js-cookie'; // Import the js-cookie library
-import {CardMedia} from "@mui/material";
+import Cookies from 'js-cookie';
+import { CardMedia } from "@mui/material";
 import Box from '@mui/material/Box';
 import RegistrationDialog from './RegisterDialog';
+
+
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage] = useState("");
-
   const [open, setOpen] = useState(false);
+
+  
 
   const handleOpen = () => {
     setOpen(true);
@@ -74,78 +77,93 @@ function Login() {
     color: 'white',
   };
 
+
+
   return (
-    <div className="App">
-      <Container id="main-container" className="d-grid h-100" >
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+    <div className="background-container">
+      <Container>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '760px',
+          }}
         >
-        <Form id="sign-in-form" className="text-center p-3 w-100"
-        style={{ maxWidth: '400px' }} 
-        onSubmit={handleLogin}>
-         <CardMedia component="img" alt="Service 1 Banner" height="140" image="wang-pic.jpg" />
-          <h1 className="mb-4 fs-3 fw-normal"        
-          style={{
-              padding: '10px',
-            }}
-            >Log Masuk</h1>
-          <Form.Group controlId="sign-in-email-address">
-            <Form.Control
-              type="email"
-              size="lg"
-              placeholder="Emel Pelajar"
-              autoComplete="username"
-              className="position-relative"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3 mt-3" controlId="sign-in-password">
-            <Form.Control
-              type="password"
-              size="lg"
-              placeholder="Kata Laluan"
-              autoComplete="current-password"
-              className="position-relative"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </Form.Group>
-          <Form.Group className="d-flex justify-content-center mb-3 #FFFFFF" controlId="remember-me">
-            <Form.Check label="Ingat Kata Laluan" />
-          </Form.Group>
-          <div className="d-grid">
-            <Button 
+          <Form id="sign-in-form" className="text-center p-3 w-100"
             style={{
-              backgroundColor: '#491E6E',
-              borderColor: '#491E6E',
-              color: 'white',
-              padding: '20px',
-              borderRadius: '10px',
+              maxWidth: '400px',
+              backgroundColor: '#FFFFFF',
+              borderRadius: '15px',
+              boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+              marginTop: '50px',
+              display: 'flex',
+              flexDirection: 'column',
             }}
-            type="submit">
+            onSubmit={handleLogin}
+          >
+            <CardMedia
+              component="img"
+              alt="Service 1 Banner"
+              height="180"
+              image="wang-pic.jpg"
+              style={{
+                borderRadius: '15px 15px 0px 0px',
+                objectFit: 'cover',
+                width: '100%',
+              }}
+            />
+            <h1 className="mb-4 fs-3 fw-normal" style={{ padding: '10px' }}>
               Log Masuk
-            </Button>
-          </div>
-          <div className="d-grid" style={{marginTop:'10px'}}>
-            <Button variant="outlined"
-            style={{
-              borderColor: '#491E6E',
-              padding: '5px',
-            }}
-            onClick={handleOpen}
-            >
-              Daftar Pengguna
-            </Button>
-            <RegistrationDialog open={open} handleClose={handleClose} />
-          </div>
-          {errorMessage && <div className="text-danger mt-3">{errorMessage}</div>}
-          <p className="mt-5 text-muted">&copy; Developed by USM Tech Team</p>
-        </Form>
+            </h1>
+            <Form.Group controlId="sign-in-email-address">
+              <Form.Control
+                type="email"
+                size="lg"
+                placeholder="Emel Pelajar"
+                autoComplete="username"
+                className="position-relative"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 mt-3" controlId="sign-in-password">
+              <Form.Control
+                type="password"
+                size="lg"
+                placeholder="Kata Laluan"
+                autoComplete="current-password"
+                className="position-relative"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </Form.Group>
+            <Form.Group className="d-flex justify-content-center mb-3 #FFFFFF" controlId="remember-me">
+              <Form.Check label="Ingat Kata Laluan" />
+            </Form.Group>
+            <div className="d-grid">
+              <Button
+                style={{
+                  backgroundColor: '#491E6E',
+                  borderColor: '#491E6E',
+                  color: 'white',
+                  padding: '20px',
+                  borderRadius: '10px',
+                }}
+                type="submit"
+              >
+                Log Masuk
+              </Button>
+            </div>
+            <div className="d-grid" style={{ marginTop: '10px' }}>
+              <Button variant="outlined" style={{ borderColor: '#491E6E', padding: '5px' }} onClick={handleOpen}>
+                Daftar Pengguna
+              </Button>
+              <RegistrationDialog open={open} handleClose={handleClose} />
+            </div>
+            {errorMessage && <div className="text-danger mt-3">{errorMessage}</div>}
+            <p className="mt-5 text-muted">&copy; Developed by USM Tech Team</p>
+          </Form>
         </Box>
       </Container>
     </div>
