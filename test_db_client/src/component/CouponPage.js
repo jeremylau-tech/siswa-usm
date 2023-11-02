b2ec3d5aaaf88756ac3ad2d28589c7f2e3808fb3
hakidon <mohdanihakim@gmail.com>
2023-11-02 22:25:14 +0800

    Coupon page has been updated

--- test_db_client/src/component/CouponPage.js
+++ test_db_client/src/component/CouponPage.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import {
   Card, CardContent, Typography, Button, Dialog, DialogTitle, DialogContent,
   DialogActions, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel,
@@ -59,14 +61,29 @@ const coupons = [
 ];
 
 function CouponPage() {
+  // const { userId } = props.location.state;
+  // const { userId } = props.location.state;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const { userId } = location.state;
+  // console.log(location.state)
+
+
   const [useDialogOpen, setUseDialogOpen] = useState(false);
   const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
   const [usedCoupons, setUsedCoupons] = useState([]);
   const [redeemIndex, setRedeemIndex] = useState(null);
   const [selectedVendor, setSelectedVendor] = useState("Vendor A");
 
-  const handleUseClick = (index) => {
-    setRedeemIndex(index);
+
+  const navToHistory = () => {
+    navigate('/HistoryFood', { state: location.state });
+  };
+
+
+  const handleUseClick = (baucarId) => {
+    setRedeemIndex(baucarId);
     setUseDialogOpen(true);
   };
 
@@ -88,52 +105,113 @@ function CouponPage() {
 
   const handleConfirmationConfirm = () => {
     // Handle the confirmation action, e.g., mark the coupon as used.
-    setUsedCoupons([...usedCoupons, redeemIndex]);
-    setConfirmationDialogOpen(false);
+    // setUsedCoupons([...usedCoupons, redeemIndex]);
+    // alert(redeemIndex)
+    // console.log(baucar)
+    // alert(baucar[redeemIndex].baucar_id)
+    const requestData = {
+      baucarId: baucar[redeemIndex].baucar_id,
+      baucarVendor: selectedVendor,
+    };
+
+    // alert(requestData.baucarId)
+  
+    fetch("http://localhost:8000/coupons-redeem", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // Handle the response data as needed
+        alert(data.message); // You can show a message from the server response
+        const updatedBaucar = baucar.filter((coupon) => coupon.baucar_id !== requestData.baucarId);
+        setBaucar(updatedBaucar);
+        setConfirmationDialogOpen(false);
+      })
+      .catch((error) => {
+        console.error("Error confirming redemption:", error);
+        setConfirmationDialogOpen(false);
+      });
   };
 
+  const [baucar, setBaucar] = useState([]);
+
+  useEffect(() => {
+    // Data to send in the request body
+    const requestData = { userId };
+    
+    fetch("http://localhost:8000/coupons-userid", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.coupons) {
+          setBaucar(data.coupons); // Assign data.coupons directly to the baucar state
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user details:", error);
+      });
+  }, []);
+
+  function formatDueDate(dueDate) {
+    const date = new Date(dueDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${day}/${month}/${year}`;
+  }
+  
+  console.log(baucar)
   return (
     <div className="coupon-page">
-      <h1 className="coupon-title">Kupon Makanan</h1>
-      <div className="coupon-container">
-        {coupons.map((coupon, index) => (
-          !usedCoupons.includes(index) && ( // Check if the coupon hasn't been used
-            <Card key={coupon.id} className="coupon-card">
-              <CardContent>
-                <Typography variant="h4" gutterBottom>
-                  {coupon.price}
-                </Typography>
-                <Typography variant="body1" className={`coupon-code`}>
-                  {coupon.code}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" style={{ marginTop: "8px" }}>
-                  {coupon.expiration}
-                </Typography>
-                <div style={{ marginTop: "12px" }}>
-                  <Button variant="outlined" color="primary" onClick={() => handleUseClick(index)}>
-                    Guna
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-          )
-        ))}
-      </div>
-
+      <h1 className="coupon-title">Kupon Makanan</h1> <Button variant="outlined" color="primary" onClick={() => navToHistory()}>
+                  Rekod Penebusan
+                </Button>
+    <div className="coupon-container">
+      {baucar.map((baucar, index) => (
+          <Card key={baucar.baucar_id} className="coupon-card">
+            <CardContent>
+              <Typography variant="h4" gutterBottom>
+                Menu Rahmah
+              </Typography>
+              <Typography variant="body1" className={`coupon-code`}>
+                {baucar.baucar_code}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" style={{ marginTop: "8px" }}>
+                {formatDueDate(baucar.baucar_due_date)}
+              </Typography>
+              <div style={{ marginTop: "12px" }}>
+                <Button variant="outlined" color="primary" onClick={() => handleUseClick(index)}>
+                  Guna
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+      ))}
+    </div>
       <Dialog open={useDialogOpen} onClose={handleUseBack}>
         <DialogTitle>Guna Kupon</DialogTitle>
         <DialogContent>
           <Typography variant="body1">Sila tunjukkan kepada vendor</Typography>
           <div style={{ textAlign: "center" }}>
             <Typography variant="body1" style={{ fontWeight: "bold" }}>
-              {coupons[redeemIndex] && coupons[redeemIndex].code}
+              {baucar[redeemIndex] && baucar[redeemIndex].baucar_code}
+              {/* baucar[redeemIndex].baucar_code */}
             </Typography>
             <Typography variant="h5" style={{ marginTop: "16px" }}>
-              {coupons[redeemIndex] && coupons[redeemIndex].price}
+              Menu Rahmah
             </Typography>
           </div>
           <FormControl component="fieldset" style={{ marginTop: "16px" }}>
-          <FormLabel component="legend">Pilih vendor</FormLabel>
+          <FormLabel component="legend">Sila Pilih Vendor</FormLabel>
           <RadioGroup
             value={selectedVendor}
             onChange={(event) => setSelectedVendor(event.target.value)}
@@ -204,7 +282,8 @@ function CouponPage() {
             <strong>Vendor Yang Dipilih:</strong> {selectedVendor}
             </Typography>
             <Typography variant="body1">
-            <strong>Kupon Makanan:</strong> {coupons[redeemIndex] && coupons[redeemIndex].price}
+            <strong>Kupon Makanan:</strong> Menu Rahmah
+            {/* <strong>Kupon Makanan:</strong> {baucar[redeemIndex] && baucar[redeemIndex].price} */}
             </Typography>
         </DialogContent>
         <DialogActions>
