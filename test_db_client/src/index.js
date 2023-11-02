c0022a10c2126564124f707ddafdba2e60567419
jeremylau-tech <jeremylau.work@gmail.com>
2023-11-02 21:40:43 +0800

    new branch

--- test_db_client/src/index.js
+++ test_db_client/src/index.js
@@ -12,6 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
+// To be released soon :)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
