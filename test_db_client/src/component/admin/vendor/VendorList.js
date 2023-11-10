25957d9362b699dce120d764b1449ade41e3942a
Firdaus K Hamid <97709951+FirdausKhamid@users.noreply.github.com>
2023-11-11 03:09:45 +0800

    Added dialog for VendorList > Rekod button

--- test_db_client/src/component/admin/vendor/VendorList.js
+++ test_db_client/src/component/admin/vendor/VendorList.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
@@ -7,6 +7,7 @@ import CreateVendor from "./EditVendor";
 import InvoicePage from "./InvoicePage"; // Import the InvoicePage component
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
+import RecordDialog from "./record/RecordDialog";
 
 
 
@@ -14,13 +15,32 @@ function VendorList() {
   const [selectedVendor, setSelectedVendor] = useState(null);
   const [showCreateVendorDialog, setShowCreateVendorDialog] = useState(false);
   const [selectedCreateVendor, setSelectedCreateVendor] = useState(null);
+  const [showRecordDialog, setShowRecordDialog] = useState(false);
+  const [recordDialogData, setRecordDialogData] = useState(null);
+
+
   const navigate = useNavigate();
   const location = useLocation();
   const [vendorMap, setVendorMap] = useState({});
 
-  const handleRekodClick = (rowId) => {
-    // Handle the Rekod button click for the row with the given ID
-    console.log(`Rekod button clicked for row with ID: ${rowId}`);
+  const handleRekodClick = (row) => {
+    // Extract the necessary information from the row
+    const { vendor_id, vendor_name, vendor_location, baucarToClaim, baucarCountRedeemed } = row;
+
+    // Create an object with the required data
+    const rowData = {
+      vendor_id,
+      vendor_name,
+      vendor_location,
+      baucarToClaim,
+      baucarCountRedeemed,
+    };
+
+    // Set the rowData to state
+    setRecordDialogData(rowData);
+
+    // Show the RecordDialog
+    setShowRecordDialog(true);
   };
 
   const handleTuntutanClick = (row) => {
@@ -84,12 +104,6 @@ function VendorList() {
       width: 150,
       editable: false,
     },
-    // {
-    //   field: "status",
-    //   headerName: "Status",
-    //   width: 100,
-    //   editable: false,
-    // },
     {
       field: "tindakan",
       headerName: "Tindakan",
@@ -113,7 +127,7 @@ function VendorList() {
             }}
             variant="contained"
             color="primary"
-            onClick={() => handleRekodClick(params.row.id)}
+            onClick={() => handleRekodClick(params.row)}
           >
             Rekod
           </Button>
@@ -160,65 +174,7 @@ function VendorList() {
         </div>
       ),
     },
-  ]; 
-
-  // const rows = [
-  //   {
-  //     id: 1,
-  //     idVendor: "VENDOR123",
-  //     VendorName: "Vendor A",
-  //     location: "Location A",
-  //     couponUsed: 10,
-  //     couponUsedTotal: 100,
-  //     status: "Active",
-  //     logoUrl: "https://img.freepik.com/premium-vector/catering-quality-food-design-logo_187482-593.jpg",
-  //     description: "Vegan Food",
-  //     owner: "Kassim Bin Ahmad",
-  //     phoneNo: "0123456789",
-  //     email: "kassim@usm.my",
-  //     registrationDate: "01/03/2022",
-  //     bank: "Maybank",
-  //     accountNo: "123456789",
-  //     accountName: "Kassim Bin Ahmad",
-  //   },
-  //   {
-  //     id: 2,
-  //     idVendor: "VENDOR456",
-  //     VendorName: "Vendor B",
-  //     location: "Location B",
-  //     couponUsed: 5,
-  //     couponUsedTotal: 5,
-  //     status: "Active",
-  //     logoUrl: "https://img.freepik.com/premium-vector/catering-quality-food-design-logo_187482-593.jpg",
-  //     description: "Burger",
-  //     owner: "Fahmi Fadzil",
-  //     phoneNo: "012232789",
-  //     email: "fahmi@usm.my",
-  //     registrationDate: "01/01/2021",
-  //     bank: "CIMB",
-  //     accountNo: "123456789",
-  //     accountName: "Shielawanis Binti Shamsuddin",
-  //   },
-  //   {
-  //     id: 3,
-  //     idVendor: "VENDOR457",
-  //     VendorName: "Vendor C",
-  //     location: "Location C",
-  //     couponUsed: 5,
-  //     couponUsedTotal: 7,
-  //     status: "Inactive",
-  //     logoUrl: "https://img.freepik.com/premium-vector/catering-quality-food-design-logo_187482-593.jpg",
-  //     description: "Nasi Kandar",
-  //     owner: "Saifuddin Bin Nasution",
-  //     phoneNo: "0123456732",
-  //     email: "saufuddin@usm.my",
-  //     registrationDate: "01/02/2021",
-  //     bank: "HSBC",
-  //     accountNo: "123456789",
-  //     accountName: "Saifuddin Bin Nasution",
-  //   },
-  //   // Add more rows with data as needed
-  // ];
+  ];
 
   const rows = Object.values(vendorMap);
   const getRowId = (row) => row.vendor_id;
@@ -226,12 +182,20 @@ function VendorList() {
   return (
     <div>
       <Box sx={{ height: 400, width: "100%" }}>
-        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection getRowId={getRowId}/>
+        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection getRowId={getRowId} />
       </Box>
       {showCreateVendorDialog && selectedCreateVendor && (
         <CreateVendor
           selectedVendor={selectedCreateVendor}
           onClose={() => setShowCreateVendorDialog(false)}
+        />
+
+      )}
+      {showRecordDialog && recordDialogData && (
+        <RecordDialog
+          open={showRecordDialog}
+          onClose={() => setShowRecordDialog(false)}
+          recordDialogData={recordDialogData}
         />
       )}
     </div>
---
+++ test_db_client/src/component/admin/vendor/record/RecordDialog.js
@@ -0,0 +1,175 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import { Typography, Box, Button, Divider } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+
+
+
+const RecordDialog = ({ open, onClose, recordDialogData }) => {
+
+    const columns = [
+        {
+            field: "invoice_id",
+            headerName: "ID Invoice",
+            width: 100,
+            editable: false,
+        },
+        {
+            field: "vendor_name",
+            headerName: "Nama Vendor",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "date_claimed",
+            headerName: "Tarikh Dituntut",
+            width: 150,
+            editable: false,
+        },
+        {
+            width: 300,
+            sortable: false,
+            renderCell: (params) => (
+                <div>
+                    <Button
+                        style={{
+                            backgroundColor: "#fafafa",
+                            color: "black",
+                            fontWeight: "bold",
+                            boxShadow: "none",
+                            outlineColor: "lightgrey",
+                            outlineStyle: "solid",
+                            outlineWidth: "1.5px",
+                            width: 80,
+                            textTransform: "none",
+                            padding: "3px",
+                            margin: "5px",
+                        }}
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleLihatClick(params.row.invoice_id)}
+                    >
+                        Lihat
+                    </Button>
+                    <Button
+                        style={{
+                            backgroundColor: "#fafafa",
+                            color: "black",
+                            fontWeight: "bold",
+                            boxShadow: "none",
+                            outlineColor: "lightgrey",
+                            outlineStyle: "solid",
+                            outlineWidth: "1.5px",
+                            width: 130,
+                            textTransform: "none",
+                            padding: "3px",
+                            margin: "5px",
+                        }}
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleSenaraiKuponClick(params.row.invoice_id)}
+                    >
+                        Senarai Kupon
+                    </Button>
+                </div>
+            ),
+        },
+    ];
+
+    const rows = [
+        {
+            invoice_id: "INV0032",
+            vendor_name: "Vendor 32",
+            date_claimed: "2021-10-01",
+            vendor_address: "Vendor 3 Address",
+            vendor_location: "Vendor 3 Location",
+            vendor_phone: "Vendor 3 Phone",
+            vendor_email: "Vendor 3 email",
+            vendor_id: "V003",
+            no_coupon_claim: 3,
+            vendor_bank_acc_name: "Ali bin Abu",
+            vendor_bank: "Maybank",
+            vendor_bank_acc_no: "21989813978139781",
+
+
+        },
+        {
+            invoice_id: "INV321213",
+            vendor_name: "Vendor 13",
+            date_claimed: "2021-10-01",
+            vendor_address: "Vendor 3 Address",
+            vendor_location: "Vendor 3 Location",
+            vendor_phone: "Vendor 3 Phone",
+            vendor_email: "Vendor 3 email",
+            vendor_id: "V003",
+            no_coupon_claim: 3,
+            vendor_bank_acc_name: "Ali bin Abu",
+            vendor_bank: "Maybank",
+            vendor_bank_acc_no: "21989813978139781",
+
+
+        },
+        {
+            invoice_id: "INV21021",
+            vendor_name: "Vendor 3",
+            date_claimed: "2021-10-01",
+            vendor_address: "Vendor 3 Address",
+            vendor_location: "Vendor 3 Location",
+            vendor_phone: "Vendor 3 Phone",
+            vendor_email: "Vendor 3 email",
+            vendor_id: "V003",
+            no_coupon_claim: 3,
+            vendor_bank_acc_name: "Ali bin Abu",
+            vendor_bank: "Maybank",
+            vendor_bank_acc_no: "21989813978139781",
+
+
+        },
+        {
+            invoice_id: "INV12331",
+            vendor_name: "Vendor 3",
+            date_claimed: "2021-10-01",
+            vendor_address: "Vendor 3 Address",
+            vendor_location: "Vendor 3 Location",
+            vendor_phone: "Vendor 3 Phone",
+            vendor_email: "Vendor 3 email",
+            vendor_id: "V003",
+            no_coupon_claim: 3,
+            vendor_bank_acc_name: "Ali bin Abu",
+            vendor_bank: "Maybank",
+            vendor_bank_acc_no: "21989813978139781",
+
+
+        },
+    ]
+
+
+    const handleLihatClick = (id) => {
+        alert("Lihat invoice " + id);
+    }
+
+    const handleSenaraiKuponClick = (id) => {
+        alert("Lihat Senarai " + recordDialogData);
+    }
+
+    const getRowId = (row) => row.invoice_id;
+
+    return (
+        <Dialog open={open} fullWidth={true} maxWidth={'md'} onClose={onClose}>
+            <DialogTitle>
+                <Typography variant='h5' style={{ textAlign: 'center', fontWeight: 'bold' }}>
+                    List of Invoices for {recordDialogData.vendor_name}
+                </Typography>
+            </DialogTitle>
+            <DialogContent>
+                <Box sx={{ height: 400, width: '100%' }}>
+                    <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection getRowId={getRowId} />
+                </Box>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default RecordDialog;
