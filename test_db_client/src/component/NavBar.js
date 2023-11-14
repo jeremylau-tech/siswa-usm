74ed4e668cb38d15a233b44fac7625e342750b0f
hakidon <mohdanihakim@gmail.com>
2023-11-14 08:55:41 +0800

    Update temp login

--- test_db_client/src/component/NavBar.js
+++ test_db_client/src/component/NavBar.js
@@ -23,7 +23,8 @@ function Navbar() {
   const handleLogout = () => {
     Cookies.remove('email');
     Cookies.remove('password');
-    navigate(`https://login.usm.my/adfs/ls/?wa=wsignin1.0&wct=${currentDate}&wtrealm=urn:federation:kebajikansiswa.usm.my/login&wctx=OmtlYmFqaWthbnNpc3dhLnVzbS5teS86`);
+    // navigate(`https://login.usm.my/adfs/ls/?wa=wsignin1.0&wct=${currentDate}&wtrealm=urn:federation:kebajikansiswa.usm.my/login&wctx=OmtlYmFqaWthbnNpc3dhLnVzbS5teS86`);
+    navigate(`/Login`);
   };
   const renderItemLinks = () => {
     if (!isResponsive) {
@@ -61,7 +62,8 @@ function Navbar() {
     } else if (!isResponsive) {
       // Render "Log Masuk" link only when not in responsive mode
       return (
-        <Link to={`https://login.usm.my/adfs/ls/?wa=wsignin1.0&wct=${currentDate}&wtrealm=urn:federation:kebajikansiswa.usm.my/login&wctx=OmtlYmFqaWthbnNpc3dhLnVzbS5teS86`}>
+        // <Link to={`https://login.usm.my/adfs/ls/?wa=wsignin1.0&wct=${currentDate}&wtrealm=urn:federation:kebajikansiswa.usm.my/login&wctx=OmtlYmFqaWthbnNpc3dhLnVzbS5teS86`}>
+        <Link to={`\Login`}>
           <Button className="p-4" style={{ color: 'white' }}>Log Masuk</Button>
         </Link>
       );
@@ -100,13 +102,14 @@ function Navbar() {
       </AppBar>
       <Drawer anchor="right" open={isResponsive && isDrawerOpen} onClose={handleDrawerToggle}>
         <List>
-          <ListItem button component="a" href="https://hac.usm.my">
+          <ListItem component="a" href="https://hac.usm.my">
             <ListItemText primary="Penginapan" />
           </ListItem>
-          <ListItem button component="a" href="https://www.instagram.com/unitkaunselingusm/?hl=en">
+          <ListItem component="a" href="https://www.instagram.com/unitkaunselingusm/?hl=en">
             <ListItemText primary="Kaunseling" />
           </ListItem>
-          <ListItem button component="a" href={`https://login.usm.my/adfs/ls/?wa=wsignin1.0&wct=${currentDate}&wtrealm=urn:federation:kebajikansiswa.usm.my/login&wctx=OmtlYmFqaWthbnNpc3dhLnVzbS5teS86`}>
+          {/* <ListItem button component="a" href={`https://login.usm.my/adfs/ls/?wa=wsignin1.0&wct=${currentDate}&wtrealm=urn:federation:kebajikansiswa.usm.my/login&wctx=OmtlYmFqaWthbnNpc3dhLnVzbS5teS86`}> */}
+          <ListItem component="a" href={`/Login`}>
             <ListItemText primary="Log Masuk" />
           </ListItem>
           <Divider />
