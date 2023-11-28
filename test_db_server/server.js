80c947b40aee3cfbf982e0447e5552697982a03f
jeremylau-tech <jeremylau.work@gmail.com>
2023-11-29 02:20:36 +0800

    up

--- test_db_server/server.js
+++ test_db_server/server.js
@@ -1221,6 +1221,6 @@ app.post("/insert-users", (req, res) => {
   });
   
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
