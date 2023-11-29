9c6d0ac5d9cc804119f9bba315c0f2037326b70e
jeremylau-tech <jeremylau.work@gmail.com>
2023-11-29 10:39:08 +0800

    ori

--- test_db_server/server.js
+++ test_db_server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require('mysql2/promise');
+const app = express();
+const mysql = require('mysql2');
 const multer = require("multer"); // For handling file uploads
 const path = require("path");
 const fs = require("fs");
@@ -9,48 +10,10 @@ const corsOptions = {
   origin: 'http://localhost:3000', // Replace with the actual origin of your frontend
   credentials: true,
 };
-
-
-const app = express();
-
-const port = 8000;
-
-app.use(cors())
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// MySQL connection configuration
-const db = mysql.createConnection({
-  host: 'bhepa_test',
-  user: 'root',
-  password: 'pelajardatabase',
-  database: 'bhepa_test',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-  // socketPath: '/var/run/mysqld/mysqld.sock'
-  });
-
-  // const db = mysql.createConnection({
-  // host: 'localhost',
-  // user: 'root',
-  // password: 'Admin@12345',
-  // database: 'bhepa_test',
-  // });
-
-
-app.get("/test-mysql", async (req, res) => {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    const [rows] = await connection.execute("SELECT 1 + 1 as result");
-    connection.end();
-
-    res.json({ message: `MySQL connection successful. Result: ${rows[0].result}` });
-  } catch (error) {
-    console.error("Error connecting to MySQL:", error);
-    res.status(500).json({ error: "MySQL connection failed." });
-  }
-});
+const port = 8000;
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 const bodyParser = require('body-parser');
@@ -70,28 +33,17 @@ function generateCouponCode(length) {
 
 let isDbConnected = false; // Variable to store the connection state
 
-//testout
-
-app.get('/omg', (req, res) => {
-  res.send('Hi There')
-});
-
-app.get('/done', (req, res) => {
-  const SelectQuery = " SELECT * FROM users";
-  db.query(SelectQuery, (err, result) => {
-    res.send(result)
-  })
-})
-
-
-app.get('/get', (req, res) => {
-  const SelectQuery = " SELECT * FROM books_reviews";
-  db.query(SelectQuery, (err, result) => {
-    res.send(result)
-  })
-})
-
 //MySQL connection configuration
+
+// MySQL connection configuration
+const db = mysql.createConnection({
+  host: 'bhepa_test',
+  user: 'root',
+  password: 'pelajardatabase',
+  database: 'bhepa_test',
+  port: 3306
+  // socketPath: '/var/run/mysqld/mysqld.sock'
+  });
 
   // socketPath: '/var/run/mysqld/mysqld.sock'
 
@@ -103,14 +55,14 @@ app.get('/get', (req, res) => {
 //   });
 
 // Connect to MySQL
-// db.connect((err) => {
-//   if (err) {
-//       console.error('Error connecting to MySQL:', err);
-//   } else {
-//     isDbConnected = true; // Set the connection state to true
-//       console.log('Connected to MySQL');
-//   }
-//   });
+db.connect((err) => {
+  if (err) {
+      console.error('Error connecting to MySQL:', err);
+  } else {
+    isDbConnected = true; // Set the connection state to true
+      console.log('Connected to MySQL');
+  }
+  });
 
 app.get("/check-db", (req, res) => {
   if (isDbConnected) {
