45e73b6aeb5e2dc72c579f8838cc1ab963e1ceea
jeremylau-tech <jeremylau.work@gmail.com>
2023-12-02 08:59:54 +0800

    up

--- sql/bhepa_test_update.sql
+++ sql/bhepa_test_update.sql
@@ -21,8 +21,8 @@
 CREATE DATABASE bhepa_test;
 USE bhepa_test;
 
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'pelajardatabase';
-FLUSH PRIVILEGES;
+-- ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'pelajardatabase';
+-- FLUSH PRIVILEGES;
 
 DROP TABLE IF EXISTS `baucar`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
