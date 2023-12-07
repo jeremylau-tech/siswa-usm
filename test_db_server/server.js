58a485281293e0824b077196fc42568c6a0f54e6
hakidon <mohdanihakim@gmail.com>
2023-12-07 13:01:15 +0800

    test

--- test_db_server/server.js
+++ test_db_server/server.js
@@ -166,6 +166,7 @@ app.post("/", (req, res) => {
     // res.json({ token, user: { unique_id: userData.matrik, email: userData.emel, roles: 'student' } });
 
     res.cookie('jwtToken', token);
+    res.cookie('test', 'asd');
     // Send the token and user information back to the client
 
     res.redirect(302, "/LoginSSO");
