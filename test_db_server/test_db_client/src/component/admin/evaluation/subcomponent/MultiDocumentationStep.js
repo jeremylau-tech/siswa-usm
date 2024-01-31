d423c5b23ed2289c68d4bfb3afd0cb2123f1190a
hakidon <mohdanihakim@gmail.com>
2024-01-31 09:08:46 +0800

    update

--- test_db_server/test_db_client/src/component/admin/evaluation/subcomponent/MultiDocumentationStep.js
+++ test_db_server/test_db_client/src/component/admin/evaluation/subcomponent/MultiDocumentationStep.js
@@ -42,15 +42,15 @@ function MultiDocumentationStep({ sponsor, requestType, justification, pdfsPath 
             value={justification}
             readOnly
           />
-          {/* <div>
+          <div>
           <Typography textAlign={"right"} padding={1}>Salinan Dokumen:</Typography>
           </div>
           <iframe
             title="Wage Certificate"
             src={`https://kebajikansiswa.usm.my/api/get-pdf?pdfPath=${pdfsPath[0].path}`}
             style={{ border:"none", width: "90%", height: "700px" }}
-          ></iframe> */}
-          <div>
+          ></iframe>
+          {/* <div>
             {pdfsPath.map((pdf, index) => (
               <div key={index}>
                 <Typography textAlign="right" padding={1}>{pdf.name}:</Typography>
@@ -61,7 +61,7 @@ function MultiDocumentationStep({ sponsor, requestType, justification, pdfsPath 
                 ></iframe>
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
       </div>
     </Container>
