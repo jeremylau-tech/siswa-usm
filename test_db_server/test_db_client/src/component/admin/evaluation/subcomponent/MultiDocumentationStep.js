3274e7ae935bb2f8dbd87a4b7c1825aa688e90df
hakidon <mohdanihakim@gmail.com>
2024-01-31 09:33:56 +0800

    update

--- test_db_server/test_db_client/src/component/admin/evaluation/subcomponent/MultiDocumentationStep.js
+++ test_db_server/test_db_client/src/component/admin/evaluation/subcomponent/MultiDocumentationStep.js
@@ -52,7 +52,17 @@ function MultiDocumentationStep({ sponsor, requestType, justification, pdfsPath 
             src={`https://kebajikansiswa.usm.my/api/get-pdf?pdfPath=${pdfsPath[0].path}`}
             style={{ border:"none", width: "90%", height: "700px" }}
           ></iframe>
-          <div></div>
+</div>
+<div>
+           <div>
+          <Typography textAlign={"right"} padding={1}>Salinan Dokumen:</Typography>
+          </div>
+          <iframe
+            title="Wage Certificate"
+            src={`https://kebajikansiswa.usm.my/api/get-pdf?pdfPath=${pdfsPath[0].path}`}
+            style={{ border:"none", width: "90%", height: "700px" }}
+          ></iframe>
+          </div>
            <div>
           <Typography textAlign={"right"} padding={1}>Salinan Dokumen:</Typography>
           </div>
@@ -69,16 +79,7 @@ function MultiDocumentationStep({ sponsor, requestType, justification, pdfsPath 
             src={`https://kebajikansiswa.usm.my/api/get-pdf?pdfPath=${pdfsPath[0].path}`}
             style={{ border:"none", width: "90%", height: "700px" }}
           ></iframe>
-           <div>
-          <Typography textAlign={"right"} padding={1}>Salinan Dokumen:</Typography>
-          </div>
-          <iframe
-            title="Wage Certificate"
-            src={`https://kebajikansiswa.usm.my/api/get-pdf?pdfPath=${pdfsPath[0].path}`}
-            style={{ border:"none", width: "90%", height: "700px" }}
-          ></iframe>
-      </div>
-    
+          
             {/* {pdfsPath.map((pdf, index) => (
                 <div>
                 <Typography textAlign="right" padding={1}>{pdf.name}:</Typography>
