de9b28226f8fce126c1cc05d838ac030ad5df58e
hakidon <mohdanihakim@gmail.com>
2024-01-31 08:30:39 +0800

    test

--- test_db_server/test_db_client/src/component/admin/evaluation/subcomponent/WangIhsanComponent.js
+++ test_db_server/test_db_client/src/component/admin/evaluation/subcomponent/WangIhsanComponent.js
@@ -125,13 +125,13 @@ useEffect(() => {
           const requestorDetails = userDetailsMap;
           const wangIhsanDetails = wangIhsanApplication[request.request_id];
           console.log(request.request_id)
-          setPdfsPath([
-            { name: 'IC Number', path:  wangIhsanDetails.ic_num_file },
-            { name: 'Bank Statement', path: wangIhsanDetails.bank_statement_file },
-            { name: 'Payment Slip (Father)', path: wangIhsanDetails.payment_slip_father_file },
-            { name: 'Payment Slip (Mother)', path: wangIhsanDetails.payment_slip_mother_file },
-            { name: 'Supporting Document', path: wangIhsanDetails.support_doc_file }
-          ]);
+          // setPdfsPath([
+          //   { name: 'IC Number', path:  wangIhsanDetails.ic_num_file },
+          //   { name: 'Bank Statement', path: wangIhsanDetails.bank_statement_file },
+          //   { name: 'Payment Slip (Father)', path: wangIhsanDetails.payment_slip_father_file },
+          //   { name: 'Payment Slip (Mother)', path: wangIhsanDetails.payment_slip_mother_file },
+          //   { name: 'Supporting Document', path: wangIhsanDetails.support_doc_file }
+          // ]);
 
           return {
             ...request,
